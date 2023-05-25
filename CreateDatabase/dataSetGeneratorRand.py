17061e6de6f8da34384bc8e5be57588411eb33bb
Joao Candido <joao.a.g.candido@tecnico.ulisboa.pt>
2023-05-25 12:41:13 +0100

    fix: this file was just a copy of grid, but now it works as intended

--- CreateDatabase/dataSetGeneratorRand.py
+++ CreateDatabase/dataSetGeneratorRand.py
@@ -7,26 +7,27 @@ import datetime
 import sys
 import os
 import logging
+import random as rnd
 
 
 parser = argparse.ArgumentParser(
-    description="Scans using pyQsc for 1st order with the following parameters: -rc1 -> -0.3, 0.3; z1 -> 0, -0.3; eta -> -3, -0.01")
+    description="Scans using pyQsc for 1st order with random values on the interval: rc1 -> 0, 0.3; z1 -> 0, -0.3; eta -> -3, -0.01\nexample:\npython3 CreateDatabase/dataSetGeneratorRand.py 10000 1 8 scan.csv.zip")
 parser.add_argument(
-    "num", help="Number of calculations for each parameter", type=int)
+    "num", help="Number of calculations for each nfp", type=int)
 parser.add_argument(
     "nfpMin", help="Starting number of field periods (nfp) for the scan", type=int)
 parser.add_argument(
     "nfpMax", help="Ending number of field periods (nfp) for the scan", type=int)
 parser.add_argument(
-    "fileName", help="Name of the file to be created")
+    "fileName", help="Name of the file to be created example scan.csv.zip")
 parser.add_argument("-v", "--verbose", action="store_true",
-                    help="Prints the duration in seconds and the progress, by showing the current number of field periods (nfp)")
+                    help="Prints the expected duration in seconds")
 args = parser.parse_args()
 
 
-def estimatedTime():
+def estimatedTime(i):
     estimatedTime = (time.time() - startTime) * \
-        input['num'] * (input['nfp'][1] - input['nfp'][0] + 1)
+        input['num'] / (i + 1)
     if args.verbose:
         print("Estimated time: ", str(
             datetime.timedelta(seconds=int(estimatedTime))))
@@ -91,9 +92,6 @@ logger.addFilter(newton_solution_fail_filter)
 # part of the radial component of the axis
 rc0 = 1
 
-# store if estimated time was printed
-timePrinted = False
-
 
 # arrays to store input
 input = {
@@ -122,55 +120,52 @@ out = {
 
 startTime = time.time()
 # this for changes nfp - number of field Periods
-for nfp in np.arange(input['nfp'][0], input['nfp'][1] + 1, 1):
-    if args.verbose:
-        print(nfp, " out of ", input['nfp'][1])
-    for rc1 in np.linspace(input['rc1'][0], input['rc1'][1], input['num']):
-        for zs1 in np.linspace(input['zs1'][0], input['zs1'][1], input['num']):
-            for eta in np.linspace(input['eta'][0], input['eta'][1], input['num']):
-                try:
-                    warningsCaptured = False
-                    # get the stellarator
-                    stel = Qsc(rc=[rc0, rc1], zs=[0, zs1], nfp=nfp, etabar=eta)
-                    if not warningsCaptured:
-                        # get output
-                        out['axLenght'].append(
-                            stel.axis_length / 2 / np.pi / rc0)
-                        out['RotTrans'].append(stel.iota)
+for i in range(args.num):
+    rc1 = rnd.uniform(input["rc1"][0], input["rc1"][1])
+    zs1 = rnd.uniform(input["zs1"][0], input["zs1"][1])
+    eta = rnd.uniform(input["eta"][0], input["eta"][1])
+    for nfp in np.arange(input['nfp'][0], input['nfp'][1] + 1, 1):
+        try:
+            warningsCaptured = False
+            # get the stellarator
+            stel = Qsc(rc=[rc0, rc1], zs=[0, zs1], nfp=nfp, etabar=eta)
+            if not warningsCaptured:
+                # get output
+                out['axLenght'].append(
+                    stel.axis_length / 2 / np.pi / rc0)
+                out['RotTrans'].append(stel.iota)
 
-                        # check if stel is QA <- heli=False or QH <- heli=True
-                        out['heli'].append(
-                            False if stel.helicity == 0 else True)
+                # check if stel is QA <- heli=False or QH <- heli=True
+                out['heli'].append(
+                    False if stel.helicity == 0 else True)
 
-                        # save parameters
-                        out['rc1'].append(rc1)
-                        out['zs1'].append(zs1)
-                        out['nfp'].append(nfp)
-                        out['eta'].append(eta)
-                        out['max_elong'].append(stel.max_elongation)
-                        out['LgradB'].append(min(stel.L_grad_B))
-                        out['min_R0'].append(stel.min_R0)
-                except KeyboardInterrupt:
-                    print('\nInterrupted')
-                    try:
-                        sys.exit(130)
-                    except SystemExit:
-                        os._exit(130)
-                except:
-                    if args.verbose:
-                        print("\nAn exception has ocurred:")
-                    saveFailedData(rc1, zs1, nfp, eta, "An exception ocurred")
+                # save parameters
+                out['rc1'].append(rc1)
+                out['zs1'].append(zs1)
+                out['nfp'].append(nfp)
+                out['eta'].append(eta)
+                out['max_elong'].append(stel.max_elongation)
+                out['LgradB'].append(min(stel.L_grad_B))
+                out['min_R0'].append(stel.min_R0)
+        except KeyboardInterrupt:
+            print('\nInterrupted')
+            try:
+                sys.exit(130)
+            except SystemExit:
+                os._exit(130)
+        except:
+            if args.verbose:
+                print("\nAn exception has ocurred:")
+            saveFailedData(rc1, zs1, nfp, eta, "An exception ocurred")
 
-        # backup data and clear out
+    # backup data and clear out
+    if (i % 500 == 0):
         out = saveData(out)
 
-        if (timePrinted == False):
-            estimatedTime()
-            timePrinted = True
+    if (i == 250):
+        estimatedTime(i)
 
 saveData(out)
-
-
 endTime = time.time() - startTime
 if args.verbose:
     print("\nEnd Time: ", str(datetime.timedelta(seconds=int(endTime))))
