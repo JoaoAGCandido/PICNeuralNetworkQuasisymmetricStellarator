e08db998fff6cde16a3865d46722494d2bb93401
Joao Candido <joao.a.g.candido@tecnico.ulisboa.pt>
2023-06-15 17:20:50 +0100

    feat: enhance seedStd for real error

--- NeuralNetwork/HyperParameter/seedStd.py
+++ NeuralNetwork/HyperParameter/seedStd.py
@@ -6,20 +6,25 @@ of the number of points"""
 
 out = {
     'numPoints': [],
-    'std': [],
+    "testR2Std": [],
+    "testR2RealStd": [],
+    "testR2RealNoOutilerStd":[],
 }
 
-#df = pd.read_csv("../nfp3Default/seedSearchDefault.csv")
-#df = pd.read_csv("../nfp3/seedSearchOptimized.csv")
-#df = pd.read_csv("../../OtherModels/seedSearchPoly.csv")
-df = pd.read_csv("../../OtherModels/seedSearchLinear.csv")
+#df = pd.read_csv("../nfp3Default/seedSearchDefault2.csv")
+#df = pd.read_csv("../nfp3/seedSearchOptimized2.csv")
+df = pd.read_csv("../../OtherModels/seedSearchPoly2.csv")
+#df = pd.read_csv("../../OtherModels/seedSearchLinear2.csv")
 
 size = len(df.index)
 for i in range(1, 100):
     out["numPoints"].append(i+1)
-    out["std"].append(df.loc[0:i, 'testR2'].std())
-
+    out["testR2Std"].append(df.loc[0:i, 'testR2'].std())
+    out["testR2RealStd"].append(df.loc[0:i, 'testR2Real'].std())
+    out["testR2RealNoOutilerStd"].append(df.loc[0:i, 'testR2RealNoOutiler'].std())
 
 df3 = pd.DataFrame(out)
 print(df3.sort_values("numPoints", ascending=False))
-df3.plot("numPoints", "std")
+df3.plot("numPoints", "testR2Std")
+df3.plot("numPoints", "testR2RealStd")
+df3.plot("numPoints", "testR2RealNoOutilerStd")
